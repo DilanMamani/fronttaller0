4a31e4cc831c7bf395bf968c8231c1d074196467
Tania Morelia Pérez Dick <160441663+TaniaPerezD@users.noreply.github.com>
2026-04-30 01:46:43 -0400

    para la hora de bloqueo

--- src/features/usuarios/Usuarios.jsx
+++ src/features/usuarios/Usuarios.jsx
@@ -282,9 +282,7 @@ export default function Usuarios() {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true,
+    
   }).format(new Date(fecha));
 };
 
